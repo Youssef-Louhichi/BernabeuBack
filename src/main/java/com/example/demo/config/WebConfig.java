71dc5595a0765ac97a2a72363323d74d3dc74414
Youssef <yousseflouhichi24@gmail.com>
2024-06-11 22:24:08 +0100

    cors changed

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://bernabeu-terrain.surge.sh")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
