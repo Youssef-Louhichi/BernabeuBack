de91f5cc97f17c16a4bdfa9424450859e27b836f
Youssef <yousseflouhichi24@gmail.com>
2024-06-12 13:37:29 +0100

    date

--- src/main/java/com/example/demo/entities/Reservation.java
+++ src/main/java/com/example/demo/entities/Reservation.java
@@ -1,7 +1,7 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
