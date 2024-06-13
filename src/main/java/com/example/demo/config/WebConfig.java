d701533c053d9a0309e0af4110c23b00ef064d68
Youssef <yousseflouhichi24@gmail.com>
2024-06-13 21:09:29 +0100

    com4

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bernabeu-terrain.surge.sh")
+                        .allowedOrigins("https://bernabeu-terrain.surge.sh","https://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
