21b5e1d8ac52c6347af8efbe2248c6a4aa981e6d
Youssef <yousseflouhichi24@gmail.com>
2024-08-07 01:42:47 +0100

    new domaine

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bernabeu-terrain.surge.sh","http://localhost:4200")
+                        .allowedOrigins("https://bernabeu-terrain.onrender.com","http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
